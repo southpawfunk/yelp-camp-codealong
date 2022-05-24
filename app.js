15073b524ad76d2d349e29f0aa64d20617db33f0
Joe Gerhardt <joe.gits.it@gmail.com>
2022-05-24 14:29:56 -0400

    install ejs-mate and begin templating. add boilerplate layout. add footer and navbar partials

--- app.js
+++ app.js
@@ -1,82 +1,82 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 //package to let us "fake" a post request as a put instead
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    /* THESE OPTIONS ARE TRUE BY DEFAULT IN MONGOOSE 6+
+mongoose.connect(
+	'mongodb://localhost:27017/yelp-camp',
+	{
+		/* THESE OPTIONS ARE TRUE BY DEFAULT IN MONGOOSE 6+
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true */
-
-});
+	}
+);
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('Database connected');
 });
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 //the name passed is the name needed in the query string to set as different request
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+	res.render('home');
 });
 
-app.get('/campgrounds', async (req,res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
+app.get('/campgrounds', async (req, res) => {
+	const campgrounds = await Campground.find({});
+	res.render('campgrounds/index', { campgrounds });
 });
-
 
 /* Order matters in routes. if the /campgrounds/new route follows the 
 /campgrounds/:id route, it will try accessing new as an id, rather than
 render the new.ejs view.
 */
 app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
+	res.render('campgrounds/new');
 });
 
 app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+	const campground = new Campground(req.body.campground);
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', {campground});
+	const campground = await Campground.findById(req.params.id);
+	res.render('campgrounds/show', { campground });
 });
 
-app.get('/campgrounds/:id/edit', async(req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {campground});
+app.get('/campgrounds/:id/edit', async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render('campgrounds/edit', { campground });
 });
 
 app.put('/campgrounds/:id', async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`);
+	const { id } = req.params;
+	const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+	res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.delete('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-})
-
-
-
-app.listen(3000, () => {
-    console.log('Serving on port 3000');
+	const { id } = req.params;
+	await Campground.findByIdAndDelete(id);
+	res.redirect('/campgrounds');
 });
 
+app.listen(3000, () => {
+	console.log('Serving on port 3000');
+});
