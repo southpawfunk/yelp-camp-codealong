3e21fda6f12a74bb63f30554fc36fadf876a7d88
Joe Gerhardt <joe.gits.it@gmail.com>
2022-05-26 12:37:56 -0400

    add fields to campground model. install axios. use unsplash api to generate random images when seeding db. add description options to seedHelpers.js. add image and description to show.ejs view.

--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const axios = require('axios');
 const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
+const apiKeys = require('../API_Keys');
+const { places, descriptors, adjectives, anecdotes, pastimes } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect(
@@ -23,13 +25,37 @@ db.once('open', () => {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+async function seedImgs() {
+	try {
+		const res = await axios.get('https://api.unsplash.com/photos/random', {
+			params : {
+				client_id   : apiKeys.unsplashKey,
+				collections : 'PHh1QTPf2ts',
+				count       : 30 //max count allowed by unsplash API
+			}
+		});
+		return res.data.map((a) => a.urls.small);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
 const seedDB = async () => {
 	await Campground.deleteMany({});
+	const imgs = await seedImgs();
 	for (let i = 0; i < 50; i++) {
+		const price = Math.ceil(Math.random() * 6) * 10 - 0.01;
 		const random1000 = Math.floor(Math.random() * 1000);
 		const camp = new Campground({
-			location : `${cities[random1000].city}, ${cities[random1000].state}`,
-			title    : `${sample(descriptors)} ${sample(places)}`
+			image       : sample(imgs),
+			location    : `${cities[random1000].city}, ${cities[random1000].state}`,
+			title       : `${sample(descriptors)} ${sample(places)}`,
+			price,
+			description :
+				`This campground is a ${sample(adjectives)} little place. ` +
+				`My family stayed there for ${Math.ceil(Math.random() * 7) + 1} nights. ` +
+				`It made me think of when ${sample(anecdotes)}. I would really like to go back ` +
+				`with my friends and ${sample(pastimes)}.`
 		});
 		await camp.save();
 	}
