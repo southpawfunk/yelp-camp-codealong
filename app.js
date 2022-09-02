a1b695e66d1ae87491f6f603a5534b581dc48d91
Joe Gerhardt <joe.gits.it@gmail.com>
2022-09-01 22:33:34 -0400

    add reviews to CampgroundSchema, add review form to show.ejs, add review creation

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ExpressError = require('./utils/ExpressError');
 //package to let us "fake" a post request as a put instead
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 mongoose.connect(
 	'mongodb://localhost:27017/yelp-camp',
@@ -108,6 +109,18 @@ app.delete(
 		const { id } = req.params;
 		await Campground.findByIdAndDelete(id);
 		res.redirect('/campgrounds');
+	})
+);
+
+app.post(
+	'/campgrounds/:id/reviews',
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		const review = new Review(req.body.review);
+		campground.reviews.push(review);
+		await review.save();
+		await campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
 	})
 );
 
